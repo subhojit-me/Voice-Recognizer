6fd2b6fccf54ad7fbd0b5d9cbcb2ec494d5a9dc9
Subhojit Khara <30930967+subhojit-me@users.noreply.github.com>
2018-04-15 22:19:07 +0530

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace Voice_Recognization
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox = new System.Windows.Forms.RichTextBox();
+            this.btn_enable = new System.Windows.Forms.Button();
+            this.btn_disable = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox
+            // 
+            this.richTextBox.Location = new System.Drawing.Point(36, 12);
+            this.richTextBox.Name = "richTextBox";
+            this.richTextBox.Size = new System.Drawing.Size(250, 196);
+            this.richTextBox.TabIndex = 0;
+            this.richTextBox.Text = "Logs";
+            // 
+            // btn_enable
+            // 
+            this.btn_enable.Location = new System.Drawing.Point(69, 226);
+            this.btn_enable.Name = "btn_enable";
+            this.btn_enable.Size = new System.Drawing.Size(179, 30);
+            this.btn_enable.TabIndex = 1;
+            this.btn_enable.Text = "Enable Voice Recognization";
+            this.btn_enable.UseVisualStyleBackColor = true;
+            this.btn_enable.Click += new System.EventHandler(this.btn_enable_Click);
+            // 
+            // btn_disable
+            // 
+            this.btn_disable.Enabled = false;
+            this.btn_disable.Location = new System.Drawing.Point(69, 275);
+            this.btn_disable.Name = "btn_disable";
+            this.btn_disable.Size = new System.Drawing.Size(179, 29);
+            this.btn_disable.TabIndex = 2;
+            this.btn_disable.Text = "Disable Voice Recognizatin";
+            this.btn_disable.UseVisualStyleBackColor = true;
+            this.btn_disable.Click += new System.EventHandler(this.btn_disable_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(329, 331);
+            this.Controls.Add(this.btn_disable);
+            this.Controls.Add(this.btn_enable);
+            this.Controls.Add(this.richTextBox);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Speech Recognization";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox;
+        private System.Windows.Forms.Button btn_enable;
+        private System.Windows.Forms.Button btn_disable;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Recognition;
+using System.Speech.Synthesis;
+
+namespace Voice_Recognization
+{
+    public partial class Form1 : Form
+    {
+        SpeechRecognitionEngine recEngine = new SpeechRecognitionEngine();
+        SpeechSynthesizer synthesizer = new SpeechSynthesizer();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_enable_Click(object sender, EventArgs e)
+        {
+            recEngine.RecognizeAsync(RecognizeMode.Multiple);
+            btn_disable.Enabled = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                Choices commands = new Choices();
+                commands.Add(new string[] { "say hello", "Print my name","Speak selected text" });
+                GrammarBuilder gbuilder = new GrammarBuilder();
+                gbuilder.Append(commands);
+                Grammar grammar = new Grammar(gbuilder);
+
+                recEngine.LoadGrammarAsync(grammar);
+                recEngine.SetInputToDefaultAudioDevice();
+                recEngine.SpeechRecognized += RecEngine_SpeechRecognized;
+            }catch(Exception ex)
+            {
+                MessageBox.Show("Error= " + ex.ToString());
+            }
+        }
+
+        private void RecEngine_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            switch (e.Result.Text)
+            {
+                case "say hello":
+                    PromptBuilder builder = new PromptBuilder();
+
+                    builder.StartSentence();
+                    builder.AppendText("Hello sir");
+                    builder.EndSentence();
+
+                    builder.AppendBreak(PromptBreak.ExtraSmall);
+
+                    builder.StartSentence();
+                    builder.AppendText("How you doing?", PromptEmphasis.Strong);
+                    builder.EndSentence();
+
+                    synthesizer.SpeakAsync(builder); 
+                    break;
+
+                case "print my name":
+                    richTextBox.Text+="\nName is = Subhojit";
+                    break;
+
+                case "Speak selected text":
+                    synthesizer.SpeakAsync(richTextBox.SelectedText);
+                    break;
+            }
+        }
+
+        private void btn_disable_Click(object sender, EventArgs e)
+        {
+            recEngine.RecognizeAsyncStop();
+            btn_disable.Enabled = false;
+
+        }
+    }
+}
